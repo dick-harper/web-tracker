3dc4dd0b75e55f220b1390cc61f02eee875acbf1
Richard Harper <dick.harper@gmail.com>
2018-04-28 09:41:50 -0600

    Update eslint

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
-  console.log('Server received request.');
-  res.send('hello...');
+  console.log('Server received request.')
+  res.send('hello...')
 };
 
 const server = http.createServer(handler);
+
+server.listen(port, err => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(`Server listening on port ${port}`)
+  }
+});
