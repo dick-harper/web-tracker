0998df801064882030c06176e75bbe722ef514ed
Richard Harper <dick.harper@gmail.com>
2018-04-29 15:40:35 -0600

    Add in figlet lib

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
+const os = require('os');
 const http = require('http');
-
+const figlet = require('figlet');
 const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
   var now = new Date;
   console.log(`Server received request from host ${req}`);
 
-  res.end(`hello...the time is ${now}, host: ${req.headers['host']}, user agent: ${req.headers['user-agent']}`);
+  figlet('Welcome to Web-Tracker!', function (err, data) {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+    //console.log(data);
+    res.end(data);
+    //res.end(`hello...${os.EOL}The time is ${now}${os.EOL}Client Address: ${req.connection.remoteAddress}${os.EOL}User Agent: ${req.headers['user-agent']}`);
+  });
+
+  
 };
 
 const server = http.createServer(handler); //.listen(port);
