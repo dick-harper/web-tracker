f5908b5a002a37882706cdb14706c085f2b282e5
Richard Harper <dick.harper@gmail.com>
2018-04-29 07:01:58 -0600

    Work on deploy to azure

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
-  console.log('Server received request.')
-  res.send('hello...')
+  var now = new Date;
+  console.log('Server received request.');
+
+  res.end(`hello...the time is ${now}`);
 };
 
 const server = http.createServer(handler);
 
 server.listen(port, err => {
   if (err) {
-    console.log(err)
+    console.log(err);
   } else {
-    console.log(`Server listening on port ${port}`)
+    console.log(`Server listening on port ${port}`);
   }
 });
